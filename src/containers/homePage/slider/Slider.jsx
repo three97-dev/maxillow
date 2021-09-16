91912e3069f06287598469ba728b9d06f9fed557
Suleman Khan <sulemank97@gmail.com>
2021-09-16 15:48:54 +0500

    home page reviews section modified

--- src/containers/homePage/slider/Slider.jsx
+++ src/containers/homePage/slider/Slider.jsx
@@ -7,17 +7,17 @@ import "./slider.scss";
 
 const Slide = ({ author, content }) => {
   return (
-    <div className="mb-[50px] lg:mb-6 home-slider-slide">
-      <div className="flex flex-col h-full justify-center">
-        <RRenderer
-          data={content}
-          config={{
-            p: "text-sm mb-4 lg:text-xl",
-          }}
-        />
-        <div className="mt-[50px]">
-          <h4 className="uppercase font-bold">{author}</h4>
+    <div className="home-slider-container mb-[50px] px-[30px] md:px-20 lg:px-0 lg:max-w-[650px] xl:max-w-[800px] 2xl:max-w-[85%] lg:mx-auto">
+      <div className="flex flex-col h-full home-slide-shadow rounded-[10px] bg-white p-[25px] pb-[30px] md:p-[50px] 2xl:px-[90px] 2xl:py-[50px]">
+        <div className="overflow-y-auto">
+          <RRenderer
+            data={content}
+            config={{
+              p: "text-sm mb-4 lg:text-xl",
+            }}
+          />
         </div>
+        <h4 className="uppercase font-bold mt-7 xl:mt-10">{author}</h4>
       </div>
     </div>
   );
@@ -25,11 +25,11 @@ const Slide = ({ author, content }) => {
 
 const ArrowRight = ({ onClick }) => {
   return (
-    <button className="home-slider-right-arrow" onClick={onClick}>
-      <div className="block lg:hidden">
-        <RightArrow size={36} color="#2FC0CC" />
-      </div>
-      <div className="hidden lg:block">
+    <button
+      className="home-slider-right-arrow mr-[30px] md:mr-20 lg:m-0"
+      onClick={onClick}
+    >
+      <div>
         <RightArrow size={50} color="#2FC0CC" />
       </div>
     </button>
@@ -38,11 +38,11 @@ const ArrowRight = ({ onClick }) => {
 
 const ArrowLeft = ({ onClick }) => {
   return (
-    <button onClick={onClick} className="home-slider-left-arrow">
-      <div className="block lg:hidden">
-        <RightArrow size={36} color="#2FC0CC" />
-      </div>
-      <div className="hidden lg:block">
+    <button
+      onClick={onClick}
+      className="home-slider-left-arrow ml-[30px] md:ml-20 lg:m-0"
+    >
+      <div>
         <RightArrow size={50} color="#2FC0CC" />
       </div>
     </button>
--- src/containers/homePage/slider/index.jsx
+++ src/containers/homePage/slider/index.jsx
@@ -6,29 +6,16 @@ function SliderSection(props) {
 
   return (
     <div
-      className="2xl:px-0 home-slider-main-container px-[30px] pt-[50px] md:px-20 lg:px-[30px]"
+      className="2xl:px-0 home-slider-main-container pt-[50px]"
       id="home-testimonials"
     >
-      <div className="2xl:max-w-[1236px] mx-auto">
-        <h3 className="text-center pb-[50px] lg:hidden">
+      <div className="relative 2xl:max-w-[1236px] mx-auto">
+        <h3 className="text-center pb-[50px] px-[30px] md:px-20 lg:px-[30px]">
           Kind Words From Our Patients
         </h3>
-        <div className="flex lg:justify-start xl:gap-7 items-end">
-          <div className="hidden lg:block relative left-[-85px]">
-            <img
-              src={props.src}
-              className="home-slider-image lg:max-w-[420px] xl:max-w-[572px]"
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="w-full lg:w-auto lg:flex-1">
-            <h3 className="hidden lg:block pb-[50px]">
-              Kind Words From Our Patients
-            </h3>
-            <div className="lg:max-w-[450px] xl:max-w-[530px] mb-[55px] lg:mb-0 lg:pb-[50px] relative">
-              <Slider slides={slides} />
-            </div>
+        <div className="pb-[55px] lg:pb-[55px]">
+          <div className="relative">
+            <Slider slides={slides} />
           </div>
         </div>
       </div>
